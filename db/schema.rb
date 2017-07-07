8778653f5ebf5bec6b5f2ed7cd8edbc1485d117f
lingboweibu1234 <qawzse656746@gmail.com>
2017-07-07 09:17:04 +0800

    add friendship index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707003711) do
+ActiveRecord::Schema.define(version: 20170707010303) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
